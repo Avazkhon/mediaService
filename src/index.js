9b96cc51dcd2dbef0c47213b7160302b54ae7910
Avazkhon <kamalxanovavazxon@gmail.com>
2021-01-15 14:50:20 +0700

    add minio

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import fileUpload from 'express-fileupload';
 import Minio from 'minio';
 
 
 const app = express();
+app.use(fileUpload());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+  useTempFiles: true,
+}));
 
 const minioClient = new Minio.Client({
-  endPoint: 'localhost',
+  endPoint: '172.17.0.1',
   port: 9000,
   useSSL: false,
   accessKey: 'khan',
-  serretKey: '0125699khan',
-})
-console.log(minioClient);
+  secretKey: 'adminKhan',
+});
 
-app.route('/')
-  .get((req, res) => {
-    res.send('Hello, i m media service!')
+app.route('/fileUpload')
+  .post((req, res) => {
+    try {
+      const metaData = {
+          'Content-Type': 'application/octet-stream',
+          'X-Amz-Meta-Testing': 1234,
+          'example': 5678
+      }
+      console.log(req.files.avatar.data);
+      minioClient.putObject('europetrip', req.files.avatar.name, req.files.avatar.data, metaData,  function(err, etag) {
+        if (err) return res.status(500).json({error: err.toString()})
+        res.status(200).json({ message: 'File uploaded successfully'})
+      });
+    } catch (error) {
+      return res.status(500).json({error: error.toString()})
+    }
   })
 
 
