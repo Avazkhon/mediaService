afdcfafdd68c5c21544d1ed800a38d6853712c99
Avazkhon <kamalxanovavazxon@gmail.com>
2021-01-16 15:15:45 +0700

    add session

--- src/use.js
+++ src/use.js
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser';
 import fileUpload from 'express-fileupload';
+import session from 'express-session';
 
 export default function (app) => {
   app.use(fileUpload());
@@ -7,5 +8,15 @@ export default function (app) => {
   app.use(bodyParser.urlencoded({
     extended: true,
     useTempFiles: true,
+  }));
+  app.use(session({
+    secret: passwords.secret,
+    saveUninitialized: false,
+    resave: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 3 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 1000 * 60 * 60 * 24 * 3
+    }),
   }));
 }
