8647e906365e9a4500c69a7f42e9b10a4f497f2a
Avazkhon <kamalxanovavazxon@gmail.com>
2021-01-19 01:33:00 +0700

    delete file use

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import fileUpload from 'express-fileupload';
+import session from 'express-session';
+import mongoStore from 'connect-mongo';
+import mongoose from 'mongoose';
+
+
+
+import db from './db.js';
 
 import image from './controller/image.js';
 
-import db from './db.js';
-import useConf from './use.js';
-
+import passwords from '../password.js';
 
 const app = express();
+const MongoStore = mongoStore(session)
 
 app.use((req, res, next) => {
   ['https://facebetting.ru', "http://localhost:3000"].map(domain => {
@@ -18,6 +26,23 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
+
+app.use(fileUpload());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+  useTempFiles: true,
+}));
+app.use(session({
+  secret: passwords.secret,
+  saveUninitialized: false,
+  resave: true,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 3 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 1000 * 60 * 60 * 24 * 3
+  }),
+}));
 
 app.route('/media/fileUpload')
   .post(image.imageUpload);
